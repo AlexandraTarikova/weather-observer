21600ba195f16156dd5ee8ac57f92bc32b9a0639
test <test@atlas.ru>
2022-04-13 15:26:18 +0300

    fix url 6

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.get('/observationSummary', function (req, res) {
   res.end(JSON.stringify(observationSummary));
 })
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
